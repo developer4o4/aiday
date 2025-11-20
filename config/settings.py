28e4c04e567021e583e3cb68b8947e8c539c56ac
Developer <adxamovizzatillo008@gmail.com>
2025-11-21 04:26:56 +0500

    permision

--- config/settings.py
+++ config/settings.py
@@ -41,19 +41,22 @@ INSTALLED_APPS = [
     'rest_framework.authtoken'
 ]
 CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
     "https://aiday.infinite-co.uz",
     "https://www.aiday.infinite-co.uz",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
-#     "http://127.0.0.1:3000",
-# ]
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://aiday.infinite-co.uz",
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
